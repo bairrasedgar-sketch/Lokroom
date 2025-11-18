cd8a37362a76479bb4340f719220d4c7893d8cb7
bairrasedgar-sketch <bairras.edgar@gmail.com>
2025-11-18 22:55:08 +0100

    Mise à jour du design et des pages

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,6 +6,15 @@ import Providers from "@/components/providers";
 export const metadata: Metadata = {
   title: "Lok’Room",
   description: "Starter",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
